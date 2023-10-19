57e217a55c7e2687e2ff5731bff9555d9787cfef
Chuma Okoro <chuma.okoro76@gmail.com>
2023-10-19 10:42:41 -0400

    add calculation if you dont make savings payments

--- src/DontSaveSingleYearPurchase.js
+++ src/DontSaveSingleYearPurchase.js
@@ -7,22 +7,34 @@ function DontSaveSingleYearPurchase() {
   const [totalDollars, setTotalDollars] = React.useState(5000);
   const [totalCost, setTotalCost] = React.useState(5000);
   const [apr, setAPR] = React.useState(10);
-  const [answer, setAnswer] = React.useState(0);
+  const [answer, setAnswer] = React.useState('?');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let periodicRate = (apr / 100) / 12
+
+    let x = Math.log(totalCost / totalDollars)
+    let y = Math.log(1 + periodicRate)
+
+    setAnswer(x/y)
+  };
 
   return (
     <div>
-      <Typography variant="h6" gutterBottom>I Have Money For A Single Year Purchase</Typography>
-      <Typography variant="body1" component={'span'} gutterBottom>
-        If I leave <Input value={totalDollars} required={true}/> dollars alone, how long will it take me 
-        to buy something that costs <Input value={totalCost} required={true}/> dollars? 
-        The annual percentage rate (APR) is <Input value={apr} required={true}/>%.
-      </Typography>
-      <br/>
-      <Button variant="outlined">Submit</Button>
-      <br/><br/>
-      <Typography variant="body1" gutterBottom>
-        ...It will take you <Input value={answer} required={true}/> months.
-      </Typography>
+      <form onSubmit={handleSubmit}>
+        <Typography variant="h6" gutterBottom>I Have Money For A Single Year Purchase</Typography>
+        <Typography variant="body1" component={'span'} gutterBottom>
+          If I leave <Input value={totalDollars} required={true} onChange={(event) => {setTotalDollars(event.target.value)}}/> dollars alone, how long will it take me
+          to buy something that costs <Input value={totalCost} required={true} onChange={(event) => {setTotalCost(event.target.value)}}/> dollars?
+          The annual percentage rate (APR) is <Input value={apr} required={true} onChange={(event) => {setAPR(event.target.value)}}/> compounded monthly%.
+        </Typography>
+        <br/>
+        <Button variant="outlined" type="submit">Submit</Button>
+        <br/><br/>
+        <Typography variant="body1" gutterBottom>
+          ...It will take you {answer} months.
+        </Typography>
+      </form>
     </div>
   );
 }
