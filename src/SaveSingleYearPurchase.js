667541e32b0fe0806b5170d8dd476fc632ef50e5
Chuma Okoro <chuma.okoro76@gmail.com>
2023-10-19 01:55:36 -0400

    remove build from gitignore

--- src/SaveSingleYearPurchase.js
+++ src/SaveSingleYearPurchase.js
@@ -9,22 +9,29 @@ function SaveSingleYearPurchase() {
   const [numMonths, setNumMonths] = React.useState(10);
   const [apr, setAPR] = React.useState(10);
   const [answer, setAnswer] = React.useState(0);
-  
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`${dollarsPerMonth * totalCost}`)
+  };
+
   return (
     <div>
-      <Typography variant="h6" gutterBottom>Save Money For A Single Year Purchase</Typography>
-      <Typography variant="body1" gutterBottom>
-        If I save <Input value={dollarsPerMonth} required={true}/> dollars every month, how long will it take me 
-        to buy something that costs <Input value={totalCost} required={true}/> dollars in <Input value={numMonths} required={true}/> months? 
-        The annual percentage rate (APR) is <Input value={apr} required={true}/>%.
-      </Typography>
-      <br/>
-      <Button variant="outlined">Submit</Button>
-      <br/>
-      <br/>
-      <Typography variant="body1" gutterBottom>
-        ...It will take you {answer} months.
-      </Typography>
+      <form onSubmit={handleSubmit}>
+        <Typography variant="h6" gutterBottom>Save Money For A Single Year Purchase</Typography>
+        <Typography variant="body1" gutterBottom>
+          If I save <Input value={dollarsPerMonth} required={true}/> dollars every month, how long will it take me
+          to buy something that costs <Input value={totalCost} required={true}/> dollars in <Input value={numMonths} required={true}/> months?
+          The annual percentage rate (APR) is <Input value={apr} required={true}/>%.
+        </Typography>
+        <br/>
+        <Button variant="outlined">Submit</Button>
+        <br/>
+        <br/>
+        <Typography variant="body1" gutterBottom>
+          ...It will take you {answer} months.
+        </Typography>
+      </form>
     </div>
   );
 }
